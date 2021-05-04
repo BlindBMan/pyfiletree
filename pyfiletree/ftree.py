039dcecb00014432d44b2c40e120072c4dbb1390
Samuel Gustin <gustin.daniel1@gmail.com>
2021-05-04 17:25:38 +0300

    Progress

--- pyfiletree/ftree.py
+++ pyfiletree/ftree.py
@@ -1,5 +1,4 @@
 from enum import Enum
-import pdb
 
 
 class Level:
@@ -15,17 +14,20 @@ class Types(Enum):
 
 
 class Node:
-    def __init__(self, value, level=Level.UNASSIGNED, debug=False, test=False):
+    def __init__(self, value, father_node=None, line=-1, level=Level.UNASSIGNED, debug=False, test=False):
         self.level = self.compute_level(value, level)
         self.value = value.lstrip()  # strip indentation
+        self.line = line + 1
         self.children = []
+        self.father = father_node
         self.type = self.compute_type()
         self.DEBUG = debug
         self.TEST = test
+        self._stop_recursion = False
 
     def __str__(self):
         if self.DEBUG:
-            return f'{self.level * 4 * " "}{self.value[:-1]} --> {self.level} : {self.type.name}'
+            return f'{self.level * 4 * " "}{self.value[:-1]} --> {self.level} : {self.father.value}'
         if self.TEST:
             return f'{self.value}'
         return f'{self.level * 4 * " "}{self.value}'
@@ -64,8 +66,18 @@ class Node:
             return Types.STATEMENT
         return Types.EMPTY_LINE
 
-    def add_child(self, child):
-        self.children.append(child)
+    def add_child(self, node):
+        self.children.append(node)
+
+    def add_children(self, children, node_to_be_replaced=None, line=-1):
+        if line == -1:
+            line_offset = self.children[-1].line + 1
+            for child in children:
+                child.update_line(line_offset)
+            self.children.extend(children)
+        else:
+            # TODO
+            pass
 
     def print_tree(self):
         end = '\n' if self.DEBUG or not self.value else ''
@@ -79,10 +91,16 @@ class Node:
         for child in self.children:
             child.get_node_list(lst)
 
+    def update_line(self, offset):
+        self.line += offset
+        for child in self.children:
+            child.update_line(offset)
+
 
 class FTree:
     def __init__(self, file_path, debug=False, test=False):
-        self.root = Node(file_path, Level.ROOT)
+        # TODO: pass a function/array of functions to apply on each node
+        self.root = Node(file_path, father_node=None, level=Level.ROOT)
         self.list_nodes = []
         self.curr_line = 0
         self._stop_recursion = False
@@ -98,7 +116,11 @@ class FTree:
 
     def _build_direct_children(self, father_node, lines):
         if self.curr_line < len(lines):
-            curr_node = Node(lines[self.curr_line], debug=self.DEBUG, test=self.TEST)
+            curr_node = Node(lines[self.curr_line],
+                             father_node=father_node,
+                             line=self.curr_line,
+                             debug=self.DEBUG,
+                             test=self.TEST)
 
             while curr_node.level >= father_node.level + 1:
                 father_node.add_child(curr_node)
@@ -106,7 +128,11 @@ class FTree:
 
                 self.curr_line += 1
                 if self.curr_line < len(lines):
-                    curr_node = Node(lines[self.curr_line], debug=self.DEBUG, test=self.TEST)
+                    curr_node = Node(lines[self.curr_line],
+                                     father_node=father_node,
+                                     line=self.curr_line,
+                                     debug=self.DEBUG,
+                                     test=self.TEST)
                     if curr_node.has_value(''):
                         curr_node.level = father_node.level + 1
                 else:
@@ -114,6 +140,8 @@ class FTree:
                     break
                 if curr_node.level > father_node.level + 1:
                     curr_node = self._build_direct_children(prev_node, lines)
+                    if curr_node:
+                        curr_node.father = father_node
                     if self._stop_recursion:
                         break
 
@@ -133,14 +161,34 @@ class FTree:
     def print_tree(self):
         self.root.print_tree()
 
-    def append(self, obj):
+    def _get_node_by_line(self, node, line):
+        if not self._stop_recursion:
+            if node.line == line:
+                self._stop_recursion = True
+                return node
+            for child in node.children:
+                found_node = self._get_node_by_line(child, line)
+                if self._stop_recursion:
+                    return found_node
+
+    def get_node_by_line(self, line):
+        self._stop_recursion = False
+        node = self._get_node_by_line(self.root, line)
+        if not node:
+            raise Exception(f'No node at line {line}')
+        return node
+
+    def append(self, obj, line=-1):
         if isinstance(obj, FTree):
-            # TODO: append logic
-            pass
+            children_to_append = obj.root.children
+            if line == -1:  # append to the end of tree: move children from root to root and update lines
+                self.root.add_children(children_to_append)
+            else:
+                curr_node = self.get_node_by_line(line)
+                curr_node.father.add_children(children_to_append, curr_node, line)
         elif isinstance(obj, str):
-            # TODO: read file + append
-            # maybe then we can reverse these ifs
-            pass
+            ftree = FTree(obj)
+            self.append(ftree, line=line)
 
     def write_to(self, path, mode='a+'):
         self._get_node_list()
